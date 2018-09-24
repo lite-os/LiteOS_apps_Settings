79a92b087eb3a78dc29f1513a12f2168e228096c
aleha-druga <aleha.druga@gmail.com>
2018-09-24 05:51:42 +0500

    Settings: add QuickSettings

---
+++ src/com/lite/settings/fragments/QuickSettings.java
@@ -0,0 +1,70 @@
+package com.lite.settings.fragments;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.os.UserHandle;
+import android.content.ContentResolver;
+import android.content.res.Resources;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v14.preference.SwitchPreference;
+import android.provider.Settings;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import java.util.Locale;
+import android.text.TextUtils;
+import android.view.View;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import com.lite.settings.preference.CustomSeekBarPreference;
+
+public class QuickSettings extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    private static final String OMNI_QS_PANEL_BG_ALPHA = "qs_panel_bg_alpha";
+
+    private CustomSeekBarPreference mQsPanelAlpha;
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        addPreferencesFromResource(R.xml.quicksettings);
+
+        PreferenceScreen prefScreen = getPreferenceScreen();
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        mQsPanelAlpha = (CustomSeekBarPreference) findPreference(OMNI_QS_PANEL_BG_ALPHA);
+        int qsPanelAlpha = Settings.System.getIntForUser(resolver,
+                Settings.System.OMNI_QS_PANEL_BG_ALPHA, 255, UserHandle.USER_CURRENT);
+        mQsPanelAlpha.setValue(qsPanelAlpha);
+        mQsPanelAlpha.setOnPreferenceChangeListener(this);
+        }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mQsPanelAlpha) {
+            int bgAlpha = (Integer) newValue;
+            Settings.System.putIntForUser(getContentResolver(),
+                    Settings.System.OMNI_QS_PANEL_BG_ALPHA, bgAlpha,
+                    UserHandle.USER_CURRENT);
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.LITE;
+    }
+
+}
